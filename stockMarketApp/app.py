0b00a33a49ce36e66b88fdbddff66732da0bfee7
SEENX02 <166158610+SEENX02@users.noreply.github.com>
2025-03-18 17:14:28 +0530

    Update app.py

--- stockMarketApp/app.py
+++ stockMarketApp/app.py
@@ -90,5 +90,8 @@ def processPredict():
     return render_template("interface.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get("PORT", 10000))  # Default to 10000
+app.run(host="0.0.0.0", port=port, debug=True)
+
 
