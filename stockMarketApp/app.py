e604fe638fb42d9e1efe8aa21a1e7fa22d80ceab
SEENX02 <166158610+SEENX02@users.noreply.github.com>
2025-03-19 14:38:00 +0530

    Update app.py

--- stockMarketApp/app.py
+++ stockMarketApp/app.py
@@ -94,6 +94,6 @@ def processPredict():
 
     return render_template("interface.html")
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000
-    app.run(host="0.0.0.0", port=port)  # ✅ Removed debug=True for production
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
